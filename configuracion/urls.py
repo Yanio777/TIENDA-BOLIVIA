71611fb0b551583603d5e0ad6705460190379f66
yanio777 <espinoza.yanio@gmail.com>
2025-12-26 21:23:47 -0400

    Desbloqueando imagenes en Render con re_path

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path # No olvides re_path
 from django.conf import settings
-from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('catalogo.urls')), # La tienda será la página principal
+    path('', include('catalogo.urls')),
 ]
 
-# Esto es vital para que las fotos de tus productos se vean en el navegador
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Esto permite que las fotos se sirvan en Render
+urlpatterns += [
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+]
