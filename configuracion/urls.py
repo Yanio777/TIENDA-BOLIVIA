d001ffc35e8336f7b931fbd760b499e5601ba6ea
yanio777 <espinoza.yanio@gmail.com>
2025-12-28 03:13:18 -0400

    Activando rutas para Google y fotos

--- configuracion/urls.py
+++ configuracion/urls.py
@@ -1,14 +1,19 @@
 from django.contrib import admin
-from django.urls import path, include, re_path # No olvides re_path
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.views.static import serve
+from django.views.generic.base import RedirectView # <--- Importante para Google
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalogo.urls')),
+    
+    # Redirecciones para que Google encuentre tus archivos en /static/
+    path('robots.txt', RedirectView.as_view(url='/static/robots.txt')),
+    path('sitemap.xml', RedirectView.as_view(url='/static/sitemap.xml')),
 ]
 
-# Esto permite que las fotos se sirvan en Render
+# Esto permite que las fotos se sirvan (Configuración para Render)
 urlpatterns += [
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
